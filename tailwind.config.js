a0f11b3d4e662830bcab4b0c5cc1ec2a66cb0246
Carly <88619398+carlyldn@users.noreply.github.com>
2023-01-13 12:38:41 +0000

    add files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,38 @@
 module.exports = {
   content: [
-    './_includes/**/*.{html,md,js}',
-    './_layouts/**/*.{html,md,js}',
-    './_*/**/*.{html,md,js}',
-    '!./_site/**/*.{html,md,js}',
-    './*.{html,md,js}'
+    './_includes/**/*.{html,md}',
+    './_layouts/**/*.{html,md}',
+    './_*/**/*.{html,md}',
+    // '!./_site/**/*.{html,md}',
+    './*.{html,md}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['Canela', 'serif'],
+      'italic-title': ['Canela Italic','serif'],
+    },
+    safelist: [
+      {
+         pattern: /grid-cols-.*/,
+         variants: ['lg, md, sm'],
+    },
+      { pattern: /col-span-.*/,
+        variants: ['lg, md, sm'],
+     },
+     { pattern: /columns-.*/,
+     variants: ['lg, md, sm'],
+  },
+    ],
+    extend: {
+      colors: {
+        beige: '#EDEBE8',
+      },
+      aspectRatio: {
+        'portrait': '3/4',
+        'landscape': '16/9',  
+        'square': '1/1',
+      },   
+    },
   },
   plugins: [],
 }
